79d2791086d1f55db4116c941bf0f647f925b551
Kartike Dutta <kartikdutta29@gmail.com>
2020-05-26 11:15:42 +0530

    rm

---
+++ build/precache-manifest.4880f30f511cb8a6df66c842f8b40044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beca5b0f0b96129012c96964b891c5fb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ae3474c843c327a92d61",
+    "url": "/portfolio/static/css/main.95704ce7.chunk.css"
+  },
+  {
+    "revision": "9bfe8649b7d03a2e1b11",
+    "url": "/portfolio/static/js/2.50a704cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.50a704cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae3474c843c327a92d61",
+    "url": "/portfolio/static/js/main.9f78643c.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "118a7642dfa7909dfc4daf6e114bacd4",
+    "url": "/portfolio/static/media/Avatar.118a7642.JPG"
+  }
+]);
